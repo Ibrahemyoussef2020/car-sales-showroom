b48ef51a83ac301ff29f49ecc4488fbb7ae0bf41
Ibrahemyoussef2020 <ibrahem.youssef2020@hotmail.com>
2024-01-07 00:48:26 +0200

    change hero car image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -31,8 +31,7 @@ const Hero = () => {
       <div className="hero__image-container">
         <div className="hero__image">
           <Image 
-           //src='/hero.png' 
-           src='/Acura.webp'
+            src='/hero.png' 
             alt="hero" 
             fill 
             className="object-contain"
