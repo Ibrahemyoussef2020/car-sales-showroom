57bfb4f849014bd5fd4b888a5662d20daaeebd15
Ibrahemyoussef2020 <ibrahem.youssef2020@hotmail.com>
2024-01-07 00:46:37 +0200

    add fn scroll

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,9 +5,13 @@ import { CostumButton } from "."
 
 const Hero = () => {
 
-  const handleScroll = ()=>{
+  const handleScroll = () => {
+    const nextSection = document.getElementById("discover");
 
-  }
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
   return (
     <div className="hero">
