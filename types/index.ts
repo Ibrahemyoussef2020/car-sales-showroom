182faa31e681a48d3bc7d433919c8e72d14e20d7
Ibrahemyoussef2020 <ibrahem.youssef2020@hotmail.com>
2024-01-06 23:19:55 +0200

    add type interfaces to all app data

---
+++ types/index.ts
@@ -0,0 +1,59 @@
+import { MouseEventHandler } from "react";
+
+export interface CostumButtonProps {
+    title:string;
+    styles?:string;
+    textStyles?:string;
+    handleClick?:MouseEventHandler<HTMLButtonElement>;
+    btnType?:'button' | 'submit';
+    rightIcon?:string;
+    isDisabled:boolean
+}
+
+export interface SearchManufacturerProps {
+    manufacturer:string;
+    setManufacturer:(manufacturer:string)=> void;
+}
+
+export interface CarProps {
+    city_mpg:number;
+    class?:string;
+    combination_mpg:number;
+    cylinders:number;
+    displacement: number;
+    drive:string;
+    fuel_type: string;
+    highway_mpg:string;
+    make:string;
+    model:string;
+    year:number,
+    transmission:string,
+    images?:string
+}
+
+export interface FilterProps {
+    manufacturer?: string;
+    year?: number;
+    model?: string;
+    limit?: number;
+    fuel?: string;
+}
+  
+  export interface HomeProps {
+    searchParams: FilterProps;
+}
+
+export interface CostumFilterOptionsProps {
+    title:string;
+    value:string
+}
+
+export interface CostumFilterProps {
+    title:string;
+    options:CostumFilterOptionsProps[]
+}
+
+export interface ShowMoreProps {
+    pageNumber:number;
+    isNext:boolean
+}
